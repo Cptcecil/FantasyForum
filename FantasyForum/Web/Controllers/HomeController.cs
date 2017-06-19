f4370154868c743f4c364bbde387ec53c7adbd90
Ovarmorrow <medeconzo@gmail.com>
2017-06-19 12:19:04 -0400

    CSS overrides, Controller Update, index update

--- FantasyForum/Web/Controllers/HomeController.cs
+++ FantasyForum/Web/Controllers/HomeController.cs
@@ -1,31 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 
 namespace Web.Controllers
 {
     public class HomeController : Controller
-    {
-        public ActionResult Index()
-        {
-			ViewBag.Message = "I love you, you.";
+	{
+		public ActionResult Index()
+		{
+			ViewBag.Message = "Forum";
 			return View();
-        }
+		}
 
-        public ActionResult About()
-        {
-            ViewBag.Message = "Forgot to make my own branch and this is the only way i know to make a change ATM.  K bye.";
+		public ActionResult About()
+		{
+			ViewBag.Message = "Forgot to make my own branch and this is the only way i know to make a change ATM.  K bye.";
 
-            return View();
-        }
+			return View();
+		}
 
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
+		public ActionResult Contact()
+		{
+			ViewBag.Message = "Your contact page.";
 
-            return View();
-        }
-    }
+			return View();
+		}
+	}
 }
