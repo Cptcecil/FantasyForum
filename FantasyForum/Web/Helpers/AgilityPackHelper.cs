7d38e9e5f478ede3380e623b423e8de0b21b3298
Dale Marshall <cptcecil@gmail.com>
2017-07-30 17:01:40 -0400

    Html Agility pack updates.

---
+++ FantasyForum/Web/Helpers/AgilityPackHelper.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using HtmlAgilityPack;
+
+namespace Web.Helpers
+{
+    public static class AgilityPackHelper
+    {
+        public static string GetFirstImageSrc(string html)
+        {
+            var doc = new HtmlDocument();
+            doc.LoadHtml(html);
+            var imgs = doc.DocumentNode.SelectNodes("//img");
+            return imgs?.FirstOrDefault()
+                .GetAttributeValue("src", null);
+        }
+    }
+}
